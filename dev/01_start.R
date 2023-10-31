3423dd7254473031e1029ebc2fb169084ab44fe5
skrendaskrenduolis <skrenda.skrenduolis@gmail.com>
2023-10-31 09:48:22 +0100

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,12 @@
 ##
 golem::fill_desc(
   pkg_name = "centraldogma28", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL, # The URL of the GitHub Repo (optional),
+  pkg_title = "centraldogma package for group28", # The Title of the package containing the App
+  pkg_description = "DNA -> RNA -> Codons -> Amino acids -> Counts", # The Description of the package containing the App
+  author_first_name = "Martynas, Attila, Karolina, Taner, Jiawen", # Your First Name
+  author_last_name = "", # Your Last Name
+  author_email = "s222858@student.dtu.dk", # Your Email
+  repo_url = "https://github.com/rforbiodatascience23/group_28_shiny.git", # The URL of the GitHub Repo (optional),
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
 
